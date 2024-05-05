dde1a9809ae8a2137362effa0bcf5e1e067ae65a
karavarichya <naerkkaravarichyan1910@mail.ru>
2024-05-05 14:18:03 +0300

    05.05 pwa

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
   plugins: [
     vue(),
     VueDevTools(),
+    VitePWA({ registerType: 'autoUpdate' }),
   ],
   resolve: {
     alias: {
