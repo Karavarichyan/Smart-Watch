4c012504455f10b24ad978130b2dc0287d5bbd84
karavarichya <naerkkaravarichyan1910@mail.ru>
2024-05-07 15:46:48 +0300

    A.Nchenge07

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,22 @@
-import { fileURLToPath, URL } from 'node:url'
-import { VitePWA } from 'vite-plugin-pwa'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import VueDevTools from 'vite-plugin-vue-devtools'
+import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import VueDevTools from "vite-plugin-vue-devtools";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-
   plugins: [
     vue(),
     VueDevTools(),
-    VitePWA({ registerType: 'autoUpdate' }),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: { name: "test", short_name: "test" },
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
