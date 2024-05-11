316a22d3645f98d6b0530a876922dadba7bdd89d
karavarichya <naerkkaravarichyan1910@mail.ru>
2024-05-11 22:20:26 +0300

    chenge fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,24 @@
+// module.exports = {
+//   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+//   darkMode: false,
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         sans: ["breeze_sans", "serif"],
+//         // 'serif': ['YourFontName', 'serif'],
+//         // 'mono': ['YourFontName', 'monospace'],
+//         // Add more font families if needed
+//       },
+//     },
+//   },
+//   variants: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
 module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  darkMode: false,
+  darkMode: 'media', // or remove it entirely if you don't need it
   theme: {
     extend: {
       fontFamily: {
